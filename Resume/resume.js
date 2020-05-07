016a1309086e1ef940904c447fccdceeb02ae60a
Devinhodge <d.hodge8891@yahoo.com>
2020-05-06 20:45:48 -0500

    created index for final portfolio

--- Resume/resume.js
+++ Resume/resume.js
@@ -1,24 +1,13 @@
 function toggleNumber() {
-  // get the clock
   var myClock = document.getElementById('number');
-
-  // get the current value of the clock's display property
   var displaySetting = myNumber.style.display;
-
-  // also get the clock button, so we can change what it says
   var numberButton = document.getElementById('numberButton');
-
-  // now toggle the clock and the button text, depending on current state
   if (displaySetting == 'block') {
-    // clock is visible. hide it
     myNumber.style.display = 'none';
-    // change button text
     numberButton.innerHTML = 'Show Number';
   }
   else {
-    // clock is hidden. show it
     myNumber.style.display = 'block';
-    // change button text
     numberButton.innerHTML = 'Hide Number';
   }
 }
---
+++ javascript-practise/checkSum/checkSum.js
@@ -0,0 +1,8 @@
+let checkSum = (num1, num2) => {
+  if (num1 === 50) {
+      return true
+  } else if ((num1 +num2) === 50) {
+      return true
+  }
+}
+console.log("50,12", checkSum(50,12))
